6f6fb7adeb67f2a50765d9f93de1b2c7b14835b2
eas-21 <easwariswathi@gmail.com>
2024-08-01 19:01:35 +0530

    property first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './core/home/home.component';
 
-const routes: Routes = [];
+import { AdminLayoutComponent } from './shared/admin-layout/admin-layout.component';
+import { VillasComponent } from './core/villas/villas.component';
 
+const routes: Routes = [
+  {path: '', component: AdminLayoutComponent,
+  children: [
+    {
+      path:'', component: HomeComponent,     
+      loadChildren: () => import('../app/core/home/home.module').then(m => m.HomeModule),      
+    },   
+    { path:'villa', component: VillasComponent},
+]
+  },];
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{
+    scrollPositionRestoration: 'top',
+    useHash: true
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminLayoutComponent } from './shared/admin-layout/admin-layout.component';
+import { HomeComponent } from './core/home/home.component';
+import { VillasComponent } from './core/villas/villas.component';
+import { ScrollTopComponent } from './shared/scroll-top/scroll-top.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,  
+    AdminLayoutComponent,
+    HomeComponent,
+    VillasComponent,
+    ScrollTopComponent
+   
   ],
   imports: [
     BrowserModule,
